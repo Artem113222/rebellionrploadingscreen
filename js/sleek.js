fac2fc71186067b7fb6e84f3ca6f014b299385b8
Artem113222 <47521538+Artem113222@users.noreply.github.com>
2019-02-11 13:43:40 +0300

    Update sleek.js

--- js/sleek.js
+++ js/sleek.js
@@ -34,14 +34,14 @@ function DownloadingFile( fileName ) {
 	downloadedFiles++;
 	refreshProgress();
 
-	setStatus("Downloading files...");
+	setStatus("Загрузка...");
 }
 
 function SetStatusChanged( status ) {
-	if (status.indexOf("Getting Addon #") != -1) {
+	if (status.indexOf("Добавление аддонов #") != -1) {
 		downloadedFiles++;
 		refreshProgress();
-	}else if (status == "Sending client info...") {
+	}else if (status == "Отправка сведений о клиенте...") {
 		setProgress(100);
 	}
 
